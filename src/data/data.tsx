2c1cfae29249d19d710770cac9e0dacdc1c9c6e1
CAGMagtibay <41831788+CAGMagtibay@users.noreply.github.com>
2023-05-23 15:35:46 -0400

    Changed: data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -59,6 +59,7 @@ export const SectionId = {
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
+  Testimonials: 'testimonials'
 } as const;
 
 export type SectionId = typeof SectionId[keyof typeof SectionId];
